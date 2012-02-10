a9d68f029f2a93f1fad3db3f0ce3ad658bb18e53
Kyle McDonald <kyle@kylemcdonald.net>
2012-02-09 19:09:08 -0500

    added slices example

---
+++ week3/SlicesExample/src/ofApp.cpp
@@ -0,0 +1,53 @@
+#include "ofApp.h"
+
+// save http://i.imgur.com/ym71g.gif as data/body.gif
+
+void ofApp::setup() {
+	ofVideoPlayer body;
+	body.loadMovie("body.gif");
+	int n = body.getTotalNumFrames();
+	for(int i = 0; i < n; i++) {
+		// load and save the current frame
+		body.setFrame(i);
+		slices.push_back(body.getPixelsRef());
+		// process each slice so the brightness determines the alpha
+		slices.back().setImageType(OF_IMAGE_COLOR_ALPHA); // RGB -> RGBA
+		ofPixels& pix = slices.back().getPixelsRef();
+		for(int y = 0; y < pix.getHeight(); y++) {
+			for(int x = 0; x < pix.getWidth(); x++) {
+				ofColor cur = pix.getColor(x, y);
+				cur.a = cur.getBrightness();
+				pix.setColor(x, y, cur);
+			}
+		}
+		// update the ofImage (upload ofPixels to ofTexture)
+		slices.back().update();
+	}
+	ofEnableAlphaBlending();
+}  
+
+void ofApp::update() {
+}
+
+void ofApp::draw() {
+	ofBackground(0);
+	cam.begin();
+	ofSetColor(255, 64); // make everything more transparent
+	float spacing = 8;
+	ofTranslate(-slices[0].getWidth() / 2, -slices[0].getHeight() / 2, -spacing * slices.size() / 2); // center things
+	for(int i = 0; i < slices.size(); i++) {
+		ofPushMatrix();
+		
+		// space the slices out evenly on the z axis
+		ofTranslate(0, 0, i * spacing);
+		
+		// jiggle things
+		float noiseOffset = ofGetElapsedTimef() + i;
+		float noiseScale = 10;
+		ofTranslate(ofSignedNoise(noiseOffset, 0) * noiseScale, ofSignedNoise(0, noiseOffset) * noiseScale, 0);
+		
+		slices[i].draw(0, 0);
+		ofPopMatrix();
+	}
+	cam.end();
+}
---
+++ week3/SlicesExample/src/ofApp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	ofEasyCam cam;
+	vector<ofImage> slices;
+};
